51b4008d6150c4c77a0613fb1e71e259054793df
Carson Rey <c.rey494@gmail.com>
2018-09-18 01:30:40 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |dwarf_names, index|
+    puts "#{index + "1".to_i} #{dwarf_names}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |element|
+    element.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+    calls.any? do |element|
+      element.size > 4
+    end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |element|
+  cheese_types.include?(element)
+  end
 end
+# check 'cheese_types' for elements in 'array', not the other way arround. Easier to search for something in what already exists.
